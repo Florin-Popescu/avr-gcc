dfc73d8af101eca61dd2af059f40a43618621eb9
Florin Popescu <florinsgpopescu@gmail.com>
2020-06-08 03:17:36 +0300

    added install script and helper files

---
+++ build_win64.sh
@@ -0,0 +1,20 @@
+INSTALL_DIR=/home/avr-gcc
+
+mkdir build
+cd build
+
+../configure \
+--prefix=$INSTALL_DIR \
+--target=avr \
+--enable-languages=c,c++ \
+--disable-nls \
+--disable-libssp \
+--disable-libada \
+--with-dwarf2 \
+--disable-shared \
+--enable-static \
+--enable-mingw-wildcard \
+--host=x86_64-w64-mingw32
+
+make all -j $NUMBER_OF_PROCESSORS
+make install
